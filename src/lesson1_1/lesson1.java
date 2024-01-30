780f5de84ea0629edbbfc469dafa47450dd60deb
klepa_82 <klepa_82@bk.ru>
2024-01-30 22:46:18 +0300

    fu

---
+++ src/lesson1_1/lesson1.java
@@ -0,0 +1,11 @@
+package lesson1_1;
+
+public class lesson1 {
+    public static void main(String[] args) {
+        fu();
+    }
+    public static void fu(){
+        System.out.println("Привет");
+    }
+
+}
